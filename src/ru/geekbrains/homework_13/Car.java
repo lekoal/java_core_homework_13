98ae25416960e92c7ea7e7090a3e9418e7fd73f5
lekoal <coreglus@gmail.com>
2021-09-19 20:32:36 +0300

    Tasks completed

--- src/ru/geekbrains/homework_13/Car.java
+++ src/ru/geekbrains/homework_13/Car.java
@@ -1,10 +1,15 @@
 package ru.geekbrains.homework_13;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
+    private static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private static boolean winnerIsHere = false; // Переменная - флаг обнаружения победителя
 
     public String getName() {
         return name;
@@ -23,15 +28,25 @@ public class Car implements Runnable {
 
     @Override
     public void run() {
+        for (int i = 0; i < race.getStages().size(); i++) {
+            race.getStages().get(i).go(this);
+
+            if (race.getStages().get(i).equals(race.getStages().get(race.getStages().size() - 1)) && !winnerIsHere) {
+                readWriteLock.writeLock().lock(); // Блокировка записи при одновременном приходе двух машин к финишу
+                System.out.println(name + " WIN"); // Вывод сообщения о победителе
+                winnerIsHere = true; // Флаг обнаружения победителя
+                readWriteLock.writeLock().unlock();
+            }
+        }
+    }
+
+    public void preparation() { // Вынесение подготовки в отдельный метод
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for (int i = 0; i < race.getStages().size(); i++) {
-            race.getStages().get(i).go(this);
         }
     }
 }
--- src/ru/geekbrains/homework_13/Main.java
+++ src/ru/geekbrains/homework_13/Main.java
@@ -1,20 +1,69 @@
 package ru.geekbrains.homework_13;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Main {
 
     public static final int CARS_COUNT = 4;
+    public static int maxThreatCount; // Переменная для хранения половины числа участников
+    public static CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+    public static CountDownLatch cdl2 = new CountDownLatch(CARS_COUNT);
+    public static Thread t1;
+    public static Thread t2;
 
     public static void main(String[] args) {
+        maxThreatCount = CARS_COUNT / 2; // Получение половины числа участников с отбрасыванием дробной части
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(new Road(60), new Tunnel(maxThreatCount), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
+
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
+
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+            int finalI = i;
+            t1 = new Thread(() -> {
+                cars[finalI].preparation();
+                cdl.countDown();
+                try {
+                    cdl.await(); // Ожидание прохождения подготовки всех участников
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            t1.start(); // Запуск подготовки
+        }
+
+        try {
+            t1.join(); // Приостановка выполнения основного потока до завершения работы t1
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+
+        for (int i = 0; i < cars.length; i++) {
+            int finalI = i;
+            t2 = new Thread(() -> {
+                cars[finalI].run();
+                cdl2.countDown();
+            });
+            t2.start(); // Старт гонки
+        }
+
+        try {
+            cdl2.await(); // Приостановка выполнения основного потока
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
